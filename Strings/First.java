f77a935ff2072e2d86be4f14d96cacf31a1c7404
user name <waghyogita07@gmail.com>
2023-10-02 23:38:31 +0530

    Strings

---
+++ Strings/First.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class First {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        // String str =  sc.next();
+        String firstName = "yogita";
+        String lastname = "wagh";
+
+        //concatenation
+        String fullName = firstName +" "+ lastname;
+        String str2 = sc.nextLine();
+
+        // System.out.println(str);
+        System.out.println(str2);
+        System.out.println(str2.length());
+        System.out.println(str2.charAt(2));
+        System.out.println(fullName);
+    }
+}
