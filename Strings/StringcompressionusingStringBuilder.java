4eb9fe41b5f03f37b02f35bb7587505d5d40576f
user name <waghyogita07@gmail.com>
2023-12-03 23:14:09 +0530

    programms

---
+++ Strings/StringcompressionusingStringBuilder.java
@@ -0,0 +1,22 @@
+public class StringcompressionusingStringBuilder {
+
+    public static String strCompression(String str){
+        StringBuilder sb = new StringBuilder("");
+        for(int i = 0; i < str.length(); i++){
+            Integer count = 1;
+            while (i < str.length()-1 && str.charAt(i) == str.charAt(i+1)) {
+                count++;
+                i++;
+            }
+            sb.append(str.charAt(i));
+            if (count > 1) {
+                sb.append(count.toString());
+            }
+        }
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        String str = "aaabbbcccdd";
+        System.out.println(strCompression(str));
+    }
+}
