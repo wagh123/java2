c8b775ec694b7f4b63e871c69a830452819b36d9
user name <waghyogita07@gmail.com>
2024-02-19 23:16:33 +0530

    bit manipulation

---
+++ Bit_Manipulation/Basics.java
@@ -0,0 +1,10 @@
+public class Basics {
+    public static void main(String[] args) {
+        System.out.println(5&6);//AND bit_wise Operator
+        System.out.println(5|6);//OR
+        System.out.println(5^6);//XOR
+        System.out.println(~0);//1's complement
+        System.out.println(5<<2);//left shift operator
+        System.out.println(6>>1);//right shift
+    }
+}
---
+++ Bit_Manipulation/OddOREven.java
@@ -0,0 +1,16 @@
+public class OddOREven {
+    public static void oddOrEven(int n){
+        int bitMask = 1;
+        if ((n & bitMask) == 0) {
+            //even
+            System.out.println("even number");
+        }else{
+            System.out.println("odd number");
+        }
+    }
+    public static void main(String[] args) {
+        oddOrEven(3);
+        oddOrEven(11);
+        oddOrEven(14);
+    }    
+}
